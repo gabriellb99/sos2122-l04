90daf77b3a4fb7959a8832e82cc70be3c74a4bb7
“gabriellb99” <gabrilopezbellido@gmail.com>
2022-04-01 12:32:27 +0200

    lab l08

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get("/api", (req,res) => {
-    console.log("Requested / route");
-    res.send({result: true})
+app.use("/", express.static("./public"));
+
+app.get("/api/v1/contacts", (req,res) => {
+    res.send([
+        {
+            name : "pablo",
+            phone : 23456
+         
+        },
+        {
+            name : "pedro",
+            phone : 12566
+        }]);
 });
 
 app.listen(port, () => {
     console.log(`Server TRULY ready at port ${port}`);
-})
+});
+
+
